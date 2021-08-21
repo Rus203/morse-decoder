65e595e048b007b0151c4ec7c7b7ba03003fac9e
Ruslan Kharkevich <MyWorkMail3103@gmail.com>
2021-08-21 18:27:31 +0300

    refactort: finish the project

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let morseCode = [];
+
+  let convertToSymbol = (nums) =>
+    {
+      let res = '';
+
+      for(let i = 0; i < nums.length; i = i + 2)
+      {
+        if(nums[i] === '1' && nums[i + 1] === '0')
+        {
+          res += '.';
+        }
+
+        else if(nums[i] === '1' && nums[i + 1] === '1')
+        {
+          res += '-';
+        }
+
+        else if (nums[i] === '*')
+        {
+          res = ' ';
+        }
+      }
+      return res;
+    }
+
+  let convertToWord = chars => 
+  {
+    return chars === ' ' ? ' ' : MORSE_TABLE[chars];
+  };
+
+  for(let x1 = 0, x2 = 10; x2 <= expr.length; x1 = x1 + 10, x2 = x2 + 10)
+  {
+    let char = expr.substring(x1, x2);
+    morseCode.push(convertToWord(convertToSymbol(char)));
+  }
+  
+  return morseCode.join('');
 }
 
 module.exports = {
